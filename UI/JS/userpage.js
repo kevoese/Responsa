2346fb4233df0118246abe4b5d7bf01c7a448a37
Esegbona Kelvin <kevoesegbona@gmail.com>
2019-02-18 20:05:00 +0100

    working on the iser profile

--- UI/JS/userpage.js
+++ UI/JS/userpage.js
@@ -1,23 +1,44 @@
+const openComment = document.querySelectorAll(".reply");
+const repliesBar = document.querySelector(".solns");
+const topic = document.querySelector(".topic");
+
 const navbar = document.querySelector(".navbar");
 const navicon = document.querySelector("#navicon");
 const navheaders = document.querySelectorAll(".navmenu h2");
 let flag = false;
+let isClick = false;
 
-navicon.addEventListener("click", e => {
-  flag = !flag;
-  if (flag) {
-    navicon.className = "close";
-    navheaders[0].parentElement.style.left = "0px";
-    navheaders.forEach(element => {
-      element.style.left = "0px";
-    });
-  } else {
-    navicon.className = "";
-    let position = 200;
-    navheaders[0].parentElement.style.left = "-25.5%";
-    navheaders.forEach(element => {
-      element.style.left = `-${position}px`;
-      position += 500;
-    });
+topic.addEventListener("click", event => {
+  if (event.target.className === "reply") {
+    let repliesbar = event.target.parentElement.parentElement.parentElement.parentElement.querySelector(
+      ".solnwrapper"
+    ).children[1];
+    isClick = !isClick;
+    if (isClick) {
+      repliesbar.style.height = "350px";
+      repliesbar.style.opacity = "1";
+    } else {
+      repliesbar.style.height = "0";
+      repliesbar.style.opacity = "0";
+    }
   }
 });
+
+// navicon.addEventListener("click", e => {
+//   flag = !flag;
+//   if (flag) {
+//     navicon.className = "close";
+//     navheaders[0].parentElement.style.left = "0px";
+//     navheaders.forEach(element => {
+//       element.style.left = "0px";
+//     });
+//   } else {
+//     navicon.className = "";
+//     let position = 200;
+//     navheaders[0].parentElement.style.left = "-25.5%";
+//     navheaders.forEach(element => {
+//       element.style.left = `-${position}px`;
+//       position += 500;
+//     });
+//   }
+// });
---
+++ UI/JS/userpage2.js
@@ -0,0 +1,263 @@
+const newtopic = document.querySelector(".topic");
+
+const imageInput = document.querySelector(".createimgfile");
+const postContenTitle = document.querySelector(".createtopictitle");
+const postButton = document.querySelector(".create");
+const postContent = document.querySelector(".createfield");
+let pictureFile = null;
+
+let newsFeed = [
+  {
+    topicID: 0,
+    title: "MPU6050",
+    topicUser: "John Doe",
+    topicDate: "Feb 5 2019",
+    userImg:
+      "http://2.bp.blogspot.com/-M4AaFPHtjgY/Up6heG0iFZI/AAAAAAAABs8/4mtrsepOqCM/s1600/Nice+Wallpapers+(13).jpg",
+    topicImg:
+      "https://cdn.pixabay.com/photo/2018/01/05/02/42/background-3062023_960_720.jpg",
+    topicContent: "lorem shit bla bla bla",
+    comments: [
+      {
+        commentUser: "Paul efe",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 0
+      },
+
+      {
+        commentUser: "Paul efe",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 1
+      },
+
+      {
+        commentUser: "Paul efe",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 2
+      }
+    ],
+    commentNo: 3
+  },
+
+  {
+    topicID: 1,
+    title: "ROUTE TO AGBARA",
+    topicUser: "Justin igugu",
+    topicDate: "Feb 15 2009",
+    userImg:
+      "http://4.bp.blogspot.com/-w3I5XQ1vWbg/Up6hKWsBRrI/AAAAAAAABss/I7_nbS_7wQ0/s1600/Nice+Wallpapers+(12).jpg",
+    topicImg:
+      "http://3.bp.blogspot.com/-2XY289Z6P7A/Up6g55D5xbI/AAAAAAAABsk/p87ndYSjTzE/s1600/Nice+Wallpapers+(10).jpg",
+    topicContent: "lorem shit bla bla bla",
+    comments: [
+      {
+        commentUser: "victor efe",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 0
+      },
+
+      {
+        commentUser: "tega amos",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 1
+      },
+
+      {
+        commentUser: "voilet justin",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 2
+      }
+    ],
+    commentNo: 3
+  },
+
+  {
+    topicID: 2,
+    title: "HBRIDGE DESIGN",
+    topicUser: "Amoslv",
+    topicDate: "Feb 5 2019",
+    userImg:
+      "http://4.bp.blogspot.com/-Yhi4Yx5G5NM/Up6hobt7o5I/AAAAAAAABtE/-puGXav1c30/s1600/Nice+Wallpapers+(1).jpg",
+    topicImg:
+      "https://cdn.pixabay.com/photo/2014/05/03/00/42/vw-camper-336606_960_720.jpg",
+    topicContent: "lorem shit bla bla bla",
+    comments: [
+      {
+        commentUser: "karo",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 0
+      },
+
+      {
+        commentUser: "kevo",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 1
+      },
+
+      {
+        commentUser: "Jude",
+        approve: 3,
+        dissapprove: 5,
+        commentContent: "some comment shits here",
+        commentID: 2
+      }
+    ],
+    commentNo: 3
+  }
+];
+
+const postUpload = post => {
+  let formContent = new FormData();
+  formContent.append("image", post.image);
+  formContent.append("name", post.image.name);
+  let data = {
+    method: "POST",
+    headers: new Headers({
+      Authorization: "Client-ID 163ceaad1d6ed26"
+    }),
+    body: formContent
+  };
+
+  fetch("https://api.imgur.com/3/image", data).then(response => {
+    response.json().then(data => {
+      console.log(data.data.link);
+      post.content.topicImg = data.data.link;
+      //make server request
+      newsFeed.push(post.content);
+      displayfeed(newsFeed);
+    });
+  });
+};
+
+imageInput.addEventListener("change", event => {
+  pictureFile = imageInput.files[0];
+});
+
+postButton.addEventListener("click", event => {
+  let date = new Date();
+  date += "";
+  date = date.split(" ");
+  date = `${date[1]} ${date[2]} ${date[3]}`;
+  let obj = {
+    topicID: 0,
+    title: postContenTitle.value,
+    topicUser: "John Doe",
+    topicDate: date,
+    userImg: "",
+    topicImg: "",
+    topicContent: postContent.value,
+    comments: []
+  };
+  let post = { image: pictureFile, content: obj };
+  postUpload(post);
+  postContent.value = "";
+  postContenTitle.value = "";
+});
+
+createElement = (element, classname) => {
+  div = document.createElement(element);
+  div.className = classname;
+  return div;
+};
+
+createComments = post => {
+  let solns = createElement("div", "solns");
+  let arr = post.comments;
+  arr.forEach(comment => {
+    let CommentDiv = createElement("div", "");
+    CommentDiv.id = post.topicID + "_" + comment.commentID;
+    let solnuser = createElement("div", "solnuser");
+    let solnusername = createElement("span", "solnusername");
+    solnusername.textContent = comment.commentUser;
+    let approve = createElement("span", "approve");
+    approve.textContent = comment.approve + "";
+    let dissapprove = createElement("span", "dissaprove");
+    dissapprove.textContent = comment.dissapprove + "";
+    solnuser.appendChild(solnusername);
+    solnuser.appendChild(approve);
+    solnuser.appendChild(dissapprove);
+    let commentText = createElement("p", "");
+    commentText.textContent = comment.commentContent;
+    CommentDiv.appendChild(solnuser);
+    CommentDiv.appendChild(commentText);
+
+    solns.appendChild(CommentDiv);
+  });
+  return solns;
+};
+
+createTopic = post => {
+  let topicContent = createElement("div", "topiccontent");
+  topicContent.id = post.title + "_" + post.topicID;
+  let topicwrapper = createElement("div", "topicwrapper");
+  let userdetail = createElement("div", "userdetail");
+  let userimg = createElement("img", "userimg");
+  userimg.src = post.userImg;
+  let userName = createElement("a", "userName");
+  userName.textContent = post.topicUser;
+  let replywrap = createElement("div", "replywrap");
+  let date = createElement("span", "date");
+  date.textContent = post.topicDate;
+  let reply = createElement("span", "reply");
+  reply.textContent = post.comments.length;
+  let title = createElement("h2", "head");
+  title.textContent = post.title;
+  let topicimg = createElement("img", "topicimg");
+  topicimg.src = post.topicImg;
+  let topictexts = createElement("p", "topictexts");
+  topictexts.textContent = post.topicContent;
+
+  let solnwrapper = createElement("div", "solnwrapper");
+  let solnbar = createElement("div", "solnbar");
+  solnbar.id = post.topicID;
+  let solution = createElement("textarea", "solution");
+  solution.placeholder = "Give a Reply...";
+  let send = createElement("div", "send");
+
+  replywrap.appendChild(date);
+  replywrap.appendChild(reply);
+
+  userdetail.appendChild(userimg);
+  userdetail.appendChild(userName);
+  userdetail.appendChild(replywrap);
+
+  topicwrapper.appendChild(userdetail);
+  topicwrapper.appendChild(title);
+  topicwrapper.appendChild(topicimg);
+  topicwrapper.appendChild(topictexts);
+
+  solnbar.appendChild(solution);
+  solnbar.appendChild(send);
+
+  solnwrapper.appendChild(solnbar);
+  solnwrapper.appendChild(createComments(post));
+
+  topicContent.appendChild(topicwrapper);
+  topicContent.appendChild(solnwrapper);
+
+  newtopic.appendChild(topicContent);
+};
+
+displayfeed = newsFeed => {
+  newsFeed.reverse().forEach(post => {
+    createTopic(post);
+  });
+};
+
+displayfeed(newsFeed);
